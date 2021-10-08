1606f15e642c9b08ee32d90463dc76dafec27df9
arthursantinati <arthursantinati02@outlook.com>
2021-10-08 14:34:32 -0300

    Exercicios do course

--- fundamentos/bloco 4 - Introdução JS/arquivo.js
+++ fundamentos/bloco 4 - Introdução JS/arquivo.js
@@ -1,7 +1,86 @@
+/*
+Exercicio 1
 const myName = "Arthur";
-const birthCity = "Americana"
-var birthYear = 2002
+const birthCity = "Americana";
+var birthYear = 2002;
+const base = 5;
+var height  = 8;
+const area = (base * height)
+const perimeter = (base + height)
+
 
 console.log(myName);
 console.log(birthCity);
 console.log(birthYear);
+console.log(area)
+console.log(perimeter*/
+
+/*
+Exercicio 2
+const nota = 85
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovado!")
+}
+else if (nota >= 60 && nota < 80) {
+    console.log("Você está na nossa lista de espera ok!")
+}
+else {
+    console.log("Você foi reprovado(a)!")
+}
+*/
+/*
+Exercicio 3
+const currentHour = 12
+var message = ""
+if (currentHour >= 22){
+    message = "Não deveríamos comer nada, é hora de dormir"
+    console.log(message)
+}
+else if (currentHour >= 18 && currentHour < 22) {
+    message = "Rango da noite, vamos jantar :D"
+    console.log(message)
+}
+else if (currentHour >= 14 && currentHour < 18){
+    message = "Vamos fazer um bolo pro café da tarde?"
+    console.log(message)
+}
+else if (currentHour > 11 && currentHour < 14){
+    message = "Hora do almoço!!!"
+    console.log(message)
+}
+else if (currentHour > 4 && currentHour < 11) {
+    message = "Hmmm, cheiro de café recém passado"
+    console.log(message)
+}
+*/
+/*
+Exercicio 4
+var weekyDay = "terça-feira"
+
+if (weekyDay === "segunda-feira" || weekyDay === "terça-feira" || weekyDay === "quarta-feira" || weekyDay === "quinta-feira" || weekyDay === "sexta-feira") {
+    console.log ("Oba, mais um dia de aprendizado na Trybe >:D")
+}
+else{
+    console.log("FINALMENTE, descanso merecido UwU")
+}
+*/
+
+/*
+Exercicio 5
+var candidato = "aprovada"
+
+switch (candidato){
+    case "aprovada":
+        console.log("Parabéns, você foi aprovado!");
+        break;
+    case "reprovada":
+        console.log("Parabéns, você foi aprovado!");
+        break;
+    case "lista":
+        console.log("Parabéns, você foi aprovado!");
+        break;
+    default: 
+        console.log("Não se aplica")
+}
+*/
+    
