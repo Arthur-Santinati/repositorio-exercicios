2ce2f9cb1a1279de44bda341eb0f3ba3bc04f899
arthursantinati <arthursantinati02@outlook.com>
2021-10-19 19:55:40 -0300

    fazendo exercicios de DOM

--- fundamentos/bloco 4 - Introdução JS/exercicios-for.js
+++ fundamentos/bloco 4 - Introdução JS/exercicios-for.js
@@ -46,3 +46,56 @@ for (var i =0; i < numbers.length; i++){
 }
 console.log("O menor número encontrado foi:", menor)*/
 
+/* ex 8 = Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado
+let num = []
+
+for (let i = 0; i <= 25 ; i += 1){
+    num.push(i);
+}
+console.log(num); */
+
+/* ex 9 = Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2
+let num =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+
+for (let i = 0; i < num.length ; i += 1){
+    console.log((num[i])/2)
+} */
+/* ex 6 = Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado"
+
+let result = 0
+for (let i = 0; i < numbers.length ; i += 1){
+    if (numbers[i] % 2 !== 0){
+        result +=1
+    }
+
+}
+
+if (result === 0){
+    console.log("nenhum valor ímpar encontrado")
+} else {
+    console.log("a quantidade de números ímpares encontrados foi de:", result, "números")
+} */
+/* ex bonus = faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n .
+let n = 10
+let inputLine = ''
+let symbol = '*'
+
+for (index = 0; index < n; index += 1){
+    inputLine = inputLine + symbol
+}
+for (index = 0; index < n; index += 1){
+    console.log(inputLine)
+} */
+
+
+let n = 10
+let lineVazio = ''
+let symbol = '*'
+
+
+for (index = 0; index < n; index += 1){
+    lineVazio = lineVazio + symbol
+}
+for (index = 0; index < n; index += 1){
+    console.log(lineVazio)
+}
